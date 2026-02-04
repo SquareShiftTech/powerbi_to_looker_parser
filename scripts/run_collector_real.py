7339bb7a29a751e1a25b473db0c0f2177bf49e43
boopathi <boopathi.jothi@squareshift.co>
2026-02-04 12:33:58 +0530

    root scripts

--- scripts/run_collector_real.py
+++ scripts/run_collector_real.py
@@ -35,7 +35,7 @@ DEFAULT_CREDENTIALS = {
 }
 DEFAULT_WORKSPACE_ID = "5945fc8b-1fb7-48a5-873a-0a35ea442166"  # Powerbi-POC
 DEFAULT_REPORT_NAME = "Suprer_Store_Dashboard"
-DEFAULT_PBIX = _REPO_ROOT / "powerbi_reports" / "Suprer_Store_Dashboard.pbix"
+DEFAULT_PBIX = _REPO_ROOT / "powerbi_to_looker" / "powerbi_reports" / "Suprer_Store_Dashboard.pbix"
 
 
 def _get_credentials() -> dict[str, str]:
